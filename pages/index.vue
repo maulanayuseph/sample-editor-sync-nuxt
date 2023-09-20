35e3919641834fc728ff11cdac709c6641cca134
Yusep Maulana <maulanayuseph@gmail.com>
2023-09-20 14:20:37 +0700

    feat: initial commit

---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <DocumentEditorContainer />
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
---
+++ plugins/documentEditor.js
@@ -0,0 +1,6 @@
+import Vue from 'vue'
+import { DocumentEditorPlugin } from '@syncfusion/ej2-vue-documenteditor'
+import { registerLicense } from '@syncfusion/ej2-base'
+
+Vue.component('DocumentEditorPlugin', DocumentEditorPlugin)
+registerLicense('Ngo9BigBOggjHTQxAR8/V1NHaF5cXmVCf1NpRGtGfV5yd0VPal1ZTndZUj0eQnxTdEZjUH5XcndVQWJbWEJ2Xg==')
